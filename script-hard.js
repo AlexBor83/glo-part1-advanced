dfb7735db856f5c47d7a8455c9482ac0e8adde83
AlexBor83 <rabota_for_alex@mail.ru>
2022-07-31 13:25:36 +0400

    репозиторий hard

---
+++ script-hard.js
@@ -0,0 +1,18 @@
+
+const num = 266219;
+const arrNums = num.toString().split('');
+
+console.log(arrNums);
+console.log(typeof arrNums);
+
+let number = 1;
+
+for (let i = 0; i < arrNums.length; i++) {   
+
+   number *= arrNums[i];    
+}
+
+console.log(number);
+const cubeNumber = number ** 3;
+
+console.log(cubeNumber.toString().slice(0,2));
