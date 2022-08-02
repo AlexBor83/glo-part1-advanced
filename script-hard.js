4553609b2983998d5cb9c18647835c1d3e321eae
AlexBor83 <rabota_for_alex@mail.ru>
2022-08-02 23:29:12 +0400

    Исправил на строгое равенство

--- script-hard.js
+++ script-hard.js
@@ -18,9 +18,9 @@ console.log(cubeNumber.toString().slice(0,2));
 
 let lang = 'en';
 
-if (lang == 'ru') {
+if (lang === 'ru') {
    console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье ');
-} else if (lang == 'en') {
+} else if (lang === 'en') {
    console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
 }
 
@@ -45,3 +45,6 @@ console.log(...weekdays[indexLangs]);
 
 let namePerson = '';
 let massage = (namePerson == 'Артем') ? console.log('директор') : (namePerson == 'Александр') ? console.log('преподаватель') : console.log('');
+
+
+
